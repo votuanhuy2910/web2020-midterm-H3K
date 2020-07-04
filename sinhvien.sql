fb2f7c6e63471ad9ca9b1ccf0472b4cdcac0c5f3
kakaka99200 <63033311+kakaka99200@users.noreply.github.com>
2020-07-04 10:57:16 +0700

    Sua lai database

--- sinhvien.sql
+++ sinhvien.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 04, 2020 lúc 05:16 AM
--- Phiên bản máy phục vụ: 10.4.11-MariaDB
--- Phiên bản PHP: 7.4.5
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2019 at 05:01 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,13 +19,32 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `sinhvien`
+-- Database: `sinhvien`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `diemthi`
+-- Table structure for table `dangnhap`
+--
+
+CREATE TABLE `dangnhap` (
+  `id` int(11) NOT NULL,
+  `account` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dangnhap`
+--
+
+INSERT INTO `dangnhap` (`id`, `account`, `password`) VALUES
+(1, 'admin', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diemthi`
 --
 
 CREATE TABLE `diemthi` (
@@ -37,63 +57,75 @@ CREATE TABLE `diemthi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Đang đổ dữ liệu cho bảng `diemthi`
+-- Dumping data for table `diemthi`
 --
 
 INSERT INTO `diemthi` (`diemthiID`, `sinhvienID`, `ltud`, `ltvm`, `htvt`, `htcm`) VALUES
 (1, 6, 8, 8.5, 8, 8),
 (2, 2, 10, 9, 8, 5),
 (3, 12, 8, 7, 8, 7),
-(4, 11, 9, 8, 1, 8),
+(4, 11, 9, 8, 8, 8),
 (5, 13, 8, 8, 9, 8);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `khoa`
+-- Table structure for table `giangvien`
 --
 
-CREATE TABLE `khoa` (
-  `makhoa` int(11) NOT NULL,
-  `tenkhoa` varchar(100) NOT NULL
+CREATE TABLE `giangvien` (
+  `idGiangvien` int(11) NOT NULL,
+  `masoGV` text NOT NULL,
+  `hoten` text NOT NULL,
+  `trinhdo` text NOT NULL,
+  `chuyenmon` text NOT NULL,
+  `email` text NOT NULL,
+  `sdt` text NOT NULL,
+  `link_avt_GV` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Đang đổ dữ liệu cho bảng `khoa`
+-- Dumping data for table `giangvien`
 --
 
-INSERT INTO `khoa` (`makhoa`, `tenkhoa`) VALUES
-(1, 'Công nghệ thông tin'),
-(2, 'Hóa');
+INSERT INTO `giangvien` (`idGiangvien`, `masoGV`, `hoten`, `trinhdo`, `chuyenmon`, `email`, `sdt`, `link_avt_GV`) VALUES
+(1, 'GV001', 'Phạm Tiến Huy', 'Th.s', 'Lập trinh', 'phamhuy@gmail.com', '168465555', 'thayhuy.jpg'),
+(2, 'GV002', 'Lê Thị Cúc ', 'Thạc sỹ ', 'Mạng máy tính ', 'lecuc@gmail.com', '0658585858', 'cocuc.jpg'),
+(3, 'GV003', 'Nguyễn Thu Thủy ', 'unknown ', 'Chủ nhiệm', 'thuthuy@mail.vn', '025454987', 'cothuy.jpg'),
+(4, 'GV004', 'Bill Gate', 'Dr. ', 'Computer Science ', 'billgate@dollar.com', '03548787878', 'billgates.jpg'),
+(5, 'GV005', 'Emma Watson', 'Tiến sỹ ', 'Computer Science , Machine Learning ', 'ema@hou.edu.vn', '0145745869', 'emma.jpg'),
+(6, 'GV006', 'Harry Potter ', 'Dr. ', 'Magic. ', 'harry@potter.com', '0909900009', 'harry.jpg'),
+(7, 'GV007', 'Vũ Song Tùng ', 'Thạc sỹ ', 'Điện tử viễn thông ', 'email@email.com', '09595959', ''),
+(9, 'GV008', 'Kudo Shinichi', 'Dr.', 'Mac - Lenin', 'kudo@gmail.com', '254875557', 'kudo.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `lophoc`
+-- Table structure for table `lophoc`
 --
 
 CREATE TABLE `lophoc` (
   `lopID` int(11) NOT NULL,
   `tenlop` text NOT NULL,
-  `phonghoc` text NOT NULL,
-  `makhoa` int(11) NOT NULL
+  `chunhiem` text NOT NULL,
+  `phonghoc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Đang đổ dữ liệu cho bảng `lophoc`
+-- Dumping data for table `lophoc`
 --
 
-INSERT INTO `lophoc` (`lopID`, `tenlop`, `phonghoc`, `makhoa`) VALUES
-(8, 'K44.CNTTA', 'P.101', 0),
-(9, 'K44.CNTT.B', 'P.101', 0),
-(10, 'K44.CNTT.C', 'P.203', 0),
-(11, 'K44.HOA.A', 'P.103', 0),
-(12, 'K44.HOA.B', 'P.301', 0);
+INSERT INTO `lophoc` (`lopID`, `tenlop`, `chunhiem`, `phonghoc`) VALUES
+(8, 'K21-A', 'Phạm Thị Lê Huyền', 'P.101'),
+(9, 'K17-A', 'Phạm Thị Lê Huyền', 'P.101'),
+(10, 'K19-A', 'Nguyễn Thu Thủy ', 'P.203'),
+(11, 'K20-A', 'Nguyễn Thu Thủy', 'P.103'),
+(12, 'K16-A', 'Phạm Thị Lê Huyền', 'P.301');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `sinhvien`
+-- Table structure for table `sinhvien`
 --
 
 CREATE TABLE `sinhvien` (
@@ -109,89 +141,101 @@ CREATE TABLE `sinhvien` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Đang đổ dữ liệu cho bảng `sinhvien`
+-- Dumping data for table `sinhvien`
 --
 
 INSERT INTO `sinhvien` (`sinhvienID`, `MaSV`, `lopID`, `name`, `birthday`, `phonenumber`, `address`, `avt`, `so_truong`) VALUES
-(2, 5, 10, 'Trịnh Đức Duy', '1998-10-30', 168656788, 'Dak Lak', '', 'football'),
-(3, 4, 8, 'Phạm Tuấn Anh', '1998-10-10', 35656565, 'Hải Phòng', '', ''),
-(5, 2, 8, 'Phạm Anh Khoa', '2000-10-15', 38456777, 'Bình Dương', '', 'football'),
-(6, 1, 10, 'Huỳnh Nhật Khánh', '2000-09-09', 1684659555, 'Vũng Tàu', 'phamhieu.jpg', 'IT chém gió !'),
+(2, 5, 10, 'Trịnh Đức Duy', '1998-10-30', 168656788, 'Thanh Hóa', '', 'football'),
+(3, 4, 8, 'Phạm Tuấn Anh', '1998-10-10', 35656565, 'Phú Yên', '', ''),
+(5, 2, 8, 'Phạm Anh Khoa', '2000-10-15', 38456777, 'Hà Nam', '', 'football'),
+(6, 1, 10, 'Phạm Viết Hiếu', '1998-09-19', 1684659555, 'Thanh Hóa', 'phamhieu.jpg', 'IT chém gió !'),
 (11, 0, 10, 'Lê Minh Hải', '1998-06-22', 95858798, 'Hà Nội', 'minhhai.jpg', 'Cua gái thần chưởng, thả thính đại pháp '),
-(12, 0, 10, 'Nguyễn Đức Duy', '2000-10-31', 38469555, 'Thanh Hóa', '', 'Art, Painting '),
-(13, 0, 11, 'Nguyễn Đức Độ ', '2000-09-27', 3922300, 'Nam Định', 'ducdo.jpg', ''),
-(14, 0, 8, 'Nguyễn Sơn Hoàng ', '2000-10-20', 35484785, 'Hồ Chí Minh', '', '');
+(12, 0, 10, 'Nguyễn Đức Duy', '1998-10-30', 38469555, 'Thanh Hóa', '', 'Art, Painting '),
+(13, 0, 11, 'Nguyễn Đức Độ ', '1998-09-27', 3922300, 'Nam Định', 'ducdo.jpg', ''),
+(14, 0, 8, 'Nguyễn Sơn Hoàng ', '2000-10-20', 35484785, 'Thanh Hóa', '', '');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `diemthi`
+-- Indexes for table `dangnhap`
+--
+ALTER TABLE `dangnhap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `diemthi`
 --
 ALTER TABLE `diemthi`
   ADD PRIMARY KEY (`diemthiID`,`sinhvienID`),
   ADD KEY `sinhvienID` (`sinhvienID`);
 
 --
--- Chỉ mục cho bảng `khoa`
+-- Indexes for table `giangvien`
 --
-ALTER TABLE `khoa`
-  ADD PRIMARY KEY (`makhoa`);
+ALTER TABLE `giangvien`
+  ADD PRIMARY KEY (`idGiangvien`);
 
 --
--- Chỉ mục cho bảng `lophoc`
+-- Indexes for table `lophoc`
 --
 ALTER TABLE `lophoc`
   ADD PRIMARY KEY (`lopID`);
 
 --
--- Chỉ mục cho bảng `sinhvien`
+-- Indexes for table `sinhvien`
 --
 ALTER TABLE `sinhvien`
   ADD PRIMARY KEY (`sinhvienID`),
   ADD KEY `lopID` (`lopID`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `diemthi`
+-- AUTO_INCREMENT for table `dangnhap`
+--
+ALTER TABLE `dangnhap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `diemthi`
 --
 ALTER TABLE `diemthi`
   MODIFY `diemthiID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT cho bảng `khoa`
+-- AUTO_INCREMENT for table `giangvien`
 --
-ALTER TABLE `khoa`
-  MODIFY `makhoa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `giangvien`
+  MODIFY `idGiangvien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT cho bảng `lophoc`
+-- AUTO_INCREMENT for table `lophoc`
 --
 ALTER TABLE `lophoc`
   MODIFY `lopID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT cho bảng `sinhvien`
+-- AUTO_INCREMENT for table `sinhvien`
 --
 ALTER TABLE `sinhvien`
   MODIFY `sinhvienID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `diemthi`
+-- Constraints for table `diemthi`
 --
 ALTER TABLE `diemthi`
   ADD CONSTRAINT `diemthi_ibfk_1` FOREIGN KEY (`sinhvienID`) REFERENCES `sinhvien` (`sinhvienID`);
 
 --
--- Các ràng buộc cho bảng `sinhvien`
+-- Constraints for table `sinhvien`
 --
 ALTER TABLE `sinhvien`
   ADD CONSTRAINT `sinhvien_ibfk_1` FOREIGN KEY (`lopID`) REFERENCES `lophoc` (`lopID`);
